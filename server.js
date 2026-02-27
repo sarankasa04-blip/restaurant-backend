c341b75d8de29a6775f27cc938ae452abb61d61b
sarankasa04-blip <sarankasa04@gmail.com>
2026-02-27 20:28:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
